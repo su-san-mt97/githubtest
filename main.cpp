09828e1b50954965cf53394955f74d58c62c6fdd
Daisuke Suzuki <48282851+su-san-mt97@users.noreply.github.com>
2023-03-02 21:30:50 +0900

    add sub function

--- main.cpp
+++ main.cpp
@@ -4,9 +4,15 @@
 void main()
 {
     printf("Hellow World! %d",add_func(10,24));
+    printf("Hellow World! %d",sub_func(10,24));
 }
 
 int add_func(int x, int y)
 {
     return (x + y);
+}
+
+int sub_func(int x, int y)
+{
+    return(x-y);
 }
