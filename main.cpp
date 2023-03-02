aee554e1b2b078765b6ca2b748c50cb8fe11eac6
Daisuke Suzuki <48282851+su-san-mt97@users.noreply.github.com>
2023-03-02 21:49:32 +0900

    reset_function

--- main.cpp
+++ main.cpp
@@ -11,25 +11,9 @@ int sub_func(int x, int y)
     return(x-y);
 }
 
-int display(char *ptr)
-{
-    printf("%s¥n");
-}
-
-bool compare(int x, int y)
-{
-    if(x < y){
-        return true;
-    } else {
-        return false;
-    }
-}
-
 void main()
 {
     printf("Hellow World! %d",add_func(10,24));
     printf("Hellow World! %d",sub_func(10,24));
-    display("This is stings.");
-    printf("result=%d¥n",compare(120, 120));
 }
 
