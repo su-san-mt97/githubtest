5186b983764e3e8a5ad4646e656f70b5cf525015
Daisuke Suzuki <48282851+su-san-mt97@users.noreply.github.com>
2023-03-02 21:46:06 +0900

    Merge pull request #3 from su-san-mt97/add_function4
    Add function4

--- main.cpp
+++ main.cpp
@@ -1,12 +1,6 @@
 #include "stdio.h"
 #define abc 123
 
-void main()
-{
-    printf("Hellow World! %d",add_func(10,24));
-    printf("Hellow World! %d",sub_func(10,24));
-}
-
 int add_func(int x, int y)
 {
     return (x + y);
@@ -15,4 +9,26 @@ int add_func(int x, int y)
 int sub_func(int x, int y)
 {
     return(x-y);
+}
+
+int display(char *ptr)
+{
+    printf("%s¥n");
+}
+
+bool compare(int x, int y)
+{
+    if(x < y){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void main()
+{
+    printf("Hellow World! %d",add_func(10,24));
+    printf("Hellow World! %d",sub_func(10,24));
+    display("This is stings.");
+    printf("result=%d¥n",compare(120, 120));
 }
